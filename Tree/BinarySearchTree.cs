bda162cd892157651b339b62e39b727a1e2dd34e
Gábor Czibik <gabor@czibik.hu>
2019-10-19 13:45:53 +0200

    BST Insert and Search

---
+++ Tree/BinarySearchTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class BinarySearchTree : BinaryTree
+    {
+        public void Insert(int data)
+        {
+            Node actual = Root;
+            Root = InsertNode(data, actual);
+        }
+
+        public bool Search(int data)
+        {
+            Node actual = Root;
+
+            while (actual != null)
+            {
+                if (actual.Data == data)
+                    return true;
+                if (actual.Data > data)
+                    actual = actual.Left;
+                else
+                    actual = actual.Right;
+            }
+
+            return false;
+        }
+
+        private Node InsertNode(int data, Node actual)
+        {
+            if (actual == null)
+            {
+                return new Node(data);
+            }
+            if (actual.Data == data)
+            {
+                return actual;
+            }
+            if (actual.Data > data)
+            {
+                var leftNode = InsertNode(data, actual.Left);
+                if (actual.Left == null)
+                {
+                    actual.Left = leftNode;
+                }
+                return actual;
+            }
+            else
+            {
+                var rightNode = InsertNode(data, actual.Right);
+                if (actual.Right == null)
+                {
+                    actual.Right = rightNode;
+                }
+                return actual;
+            }
+        }
+    }
+}
--- Tree/Program.cs
+++ Tree/Program.cs
@@ -7,11 +7,32 @@ namespace Tree
         static void Main(string[] args)
         {
             var binaryTree = new BinaryTree();
-
             binaryTree.Root = new Node(1);
             binaryTree.Root.Left = new Node(2);
             binaryTree.Root.Right = new Node(3);
             binaryTree.Root.Left.Left = new Node(4);
+
+            /*
+               The left subtree of a node contains only nodes with keys lesser than the node’s key.
+               The right subtree of a node contains only nodes with keys greater than the node’s key.
+               The left and right subtree each must also be a binary search tree.
+               There must be no duplicate nodes.
+            */
+            var binarySearchTree = new BinarySearchTree();
+            binarySearchTree.Insert(8);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(1);
+            binarySearchTree.Insert(6);
+            binarySearchTree.Insert(4);
+            binarySearchTree.Insert(7);
+            binarySearchTree.Insert(10);
+            binarySearchTree.Insert(14);
+            binarySearchTree.Insert(13);
+
+            int searchFor = 7;
+            Console.WriteLine($"{searchFor} exists in BST: {binarySearchTree.Search(searchFor)}");
+            searchFor = 17;
+            Console.WriteLine($"{searchFor} exists in BST: {binarySearchTree.Search(searchFor)}");
         }
     }
 }
