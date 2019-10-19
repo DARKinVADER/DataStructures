b7ea8e3772f879b0063467182ff4fb117d112118
Gábor Czibik <gabor@czibik.hu>
2019-10-19 13:18:27 +0200

    BinaryTree

---
+++ Tree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binaryTree = new BinaryTree();
+
+            binaryTree.Root = new Node(1);
+            binaryTree.Root.Left = new Node(2);
+            binaryTree.Root.Right = new Node(3);
+            binaryTree.Root.Left.Left = new Node(4);
+        }
+    }
+}
